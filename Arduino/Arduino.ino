d6caaab062548fee423781ea9d26406d70045ebb
JelleWho <1545223+jellewie@users.noreply.github.com>
2019-12-14 10:58:33 +0100

    remove def notes

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -1,15 +1,6 @@
 /*
   Program written by JelleWho, based on DIYHUE ESP8266
   Board: https://dl.espressif.com/dl/package_esp32_index.json
-
-
-  IFTT SETTINGS:
-  http://77.173.36.55/  POST  application/x-www-form-urlencoded   A?light=5
-  ->  Method: 2 URI: /    arg 'A?light' = '5'
-
-  http://77.173.36.55/gateways/0xF400/rgb/1   post  plain   "status": true
-  ->  Method: 2 URI: /gateways/0xF400/rgb/1   arg 'plain' = '"status": true'
-
 */
 #include <WiFi.h>
 
